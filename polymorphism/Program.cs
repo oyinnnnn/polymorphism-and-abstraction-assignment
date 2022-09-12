0d6a2065a1bd4bc1118228907e4a7040633d3d40
pc <pc@DESKTOP-P40ILKJ>
2022-09-12 02:36:15 +0100

    Add project files.

---
+++ polymorphism/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism
+{
+    internal class StudentPortal
+    {
+        string LoginDetails(int matricno, string password)
+        {
+
+            //return "A-009344/NGN/KEB/0093";
+            return System.Guid.NewGuid().ToString();
+        }
+
+        string LoginDetails(int registrationnumber)
+
+
+            { return System.Guid.NewGuid().ToString(); 
+        }
+        
+        string LoginDetails(string studentfirstname, string studentlastname)
+
+
+            { return System.Guid.NewGuid().ToString(); 
+        }
+
+        string LoginDetails(int IDnumber, int classno)
+            
+        
+               { return System.Guid.NewGuid().ToString(); 
+        }
+
+        string LoginDetails(StudentDetails studentDetails)
+        {
+            //code to reach the school
+            string transcript = studentDetails.transcript;
+            return System.Guid.NewGuid().ToString();
+        }
+            
+    }
+
+    internal class StudentDetails
+    {
+        public string transcript;
+    }
+}
---
+++ polymorphism/StudentPortalBase.cs
@@ -0,0 +1,8 @@
+﻿namespace polymorphism
+{
+    internal class StudentPortalBase
+    {
+
+        private string LoginDetails(int idnumber  int classnumber);
+    }
+}
